a48acf6821d9df7212baa5c1247e57183dce3281
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-28 17:26:36 +0200

    Bonus ivelli di difficolta

--- main.js
+++ main.js
@@ -7,28 +7,82 @@ function creazioneQaudrato(x) {
     return div;
 }
 
+let difficolta;
+
+
+
 function play() {
 
+    difficolta = document.getElementById("dificultSelection").value;
+    console.log(difficolta)
     const griglia = document.getElementById("areaDiGioco");
 
-    for (let i = 0; i < 100; i++) {
-        
-        // Creazione del quadrato
-        let activeElement = creazioneQaudrato(i + 1);
+    if (difficolta == "easy") {
+        for (let i = 0; i < 100; i++) {
 
-        // Rendo il quadrato cliccabile aggiungendogli la classe
-        activeElement.addEventListener('click', function () {
-            this.classList.toggle('quadratoSelezionato');
+            // Creazione del quadrato
+            let activeElement = creazioneQaudrato(i + 1);
 
-            let numeroCliccato = this.innerText;
-            
-            console.log(numeroCliccato);
+            // Rendo il quadrato cliccabile aggiungendogli la classe
+            activeElement.addEventListener('click', function () {
+                this.classList.toggle('quadratoSelezionato');
+                let numeroCliccato = this.innerText;
+                console.log(numeroCliccato);
 
-        })
-        
+            })
 
-        griglia.append(activeElement);
+
+            griglia.append(activeElement);
+        }
+
     }
+    else if (difficolta == "normal") {
+
+        for (let i = 0; i < 81; i++) {
+
+            // Creazione del quadrato
+            let activeElement = creazioneQaudrato(i + 1);
+            activeElement.classList.remove("quadrato");
+            activeElement.classList.add("quadratoLevelNormal");
+
+            // Rendo il quadrato cliccabile aggiungendogli la classe
+            activeElement.addEventListener('click', function () {
+
+                this.classList.toggle('quadratoSelezionato');
+
+                let numeroCliccato = this.innerText;
+                console.log(numeroCliccato);
+
+            })
+
+
+            griglia.append(activeElement);
+        }
+    }
+    else {
+
+        for (let i = 0; i < 49; i++) {
+
+            // Creazione del quadrato
+            let activeElement = creazioneQaudrato(i + 1);
+            activeElement.classList.remove("quadrato");
+            activeElement.classList.add("quadratoLevelHard");
+
+            // Rendo il quadrato cliccabile aggiungendogli la classe
+            activeElement.addEventListener('click', function () {
+
+                this.classList.toggle('quadratoSelezionato');
+
+                let numeroCliccato = this.innerText;
+                console.log(numeroCliccato);
+
+            })
+
+
+            griglia.append(activeElement);
+        }
+    }
+
 
 
 }
