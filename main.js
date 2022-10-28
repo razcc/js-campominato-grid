db369e564cd0ace1ff9d3a62b05b7970bfa444c5
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-28 16:10:15 +0200

    Stampa dei numeri a video

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-function creazioneQaudrato() {
+let controllo = false;
+
+function creazioneQaudrato(x) {
     const div = document.createElement("div");
     div.classList.add("quadrato");
+    div.innerText = x;
     return div;
 }
 
 function play() {
+    controllo = true;
     const griglia = document.getElementById("areaDiGioco");
 
     for (let i = 0; i < 100; i++) {
+
         // Creazione del quadrato
-        let activeElement = creazioneQaudrato();
+        let activeElement = creazioneQaudrato(i+1);
 
         // Rendo il quadrato cliccabile aggiungendogli la classe
         activeElement.addEventListener('click', function () {
